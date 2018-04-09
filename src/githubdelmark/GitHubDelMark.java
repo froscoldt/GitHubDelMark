d0606b334d44f640d770080906d9040e743ee9f0
Mark <Mark@10.50.130.18>
2018-04-09 09:02:35 +0200

    added value

--- src/githubdelmark/GitHubDelMark.java
+++ src/githubdelmark/GitHubDelMark.java
@@ -17,7 +17,7 @@ public class GitHubDelMark {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        int life = 0;
         //hej
     }
     
